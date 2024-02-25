4394e912a65d168a72b34aa91b5904adf0171241
mostaq786 <60430930+mostaq786@users.noreply.github.com>
2024-02-25 20:42:14 +0530

    Add files via upload

---
+++ mynodeapp.js
@@ -0,0 +1,7 @@
+const http=require("http");
+const server=http.createServer((req, res)=>{
+	res.writeHead(200, {"Content-Type":"text/plain"});
+	res.end("Hello World!!!");
+});
+const port=process.env.PORT || 3000;
+server.listen(port, ()=>console.log("Server is runnng on port 3000"));
